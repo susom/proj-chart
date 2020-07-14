51a008a6fd79a11b4c0c03ff30d03c90c85d954b
Ihab Zeedia <ihab.zeedia@gmail.cm>
2020-07-14 15:47:49 -0700

    fix few css errors.

--- pages/verification_form.php
+++ pages/verification_form.php
@@ -6,6 +6,7 @@ namespace Stanford\ProjChart;
 /** @var \Stanford\ProjChart\ProjChart $this */
 
 ?>
+<link rel="stylesheet" type="text/css" href="<?php echo $this->getUrl('asset/css/verification_form.css') ?>">
 <script src="<?php echo $this->getUrl('asset/js/verification_form.js') ?>"></script>
 <script>
     Form.ajaxURL = "<?php echo $this->getUrl('ajax/verify.php', false, true) ?>"
